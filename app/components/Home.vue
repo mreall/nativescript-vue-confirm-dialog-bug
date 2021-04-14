99c8deb6c28ebd02db4676cc5097cf9dd9dff9ec
Mike Reall <mreall@incubug.com>
2021-04-14 18:21:50 -0500

    Demonstrates confirm dialog issue on iOS

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,57 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <Label text="Home" />
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <GridLayout>
+      <Label class="info">
+        <FormattedString>
+          <Span class="fas" text.decode="&#xf135; " />
+          <Span :text="message" />
+        </FormattedString>
+      </Label>
+      <Button text="Open confirm dialog" @tap="onTap" />
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+export default {
+  computed: {
+    message() {
+      return "Blank {N}-Vue app";
+    },
+  },
+
+  methods: {
+    onTap() {
+      console.log(`Confirm dialog should open now.`);
+      confirm({
+        title: "Confirm",
+        message: "Does the dialog appear?",
+        okButtonText: "Yes",
+        cancelButtonText: "No",
+        cancelable: true,
+      }).then((res) => {
+        console.log(`Confirm dialog closed.`);
+      });
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+@import "@nativescript/theme/scss/variables/blue";
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+  @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+  font-size: 20;
+  horizontal-align: center;
+  vertical-align: center;
+}
 </style>
